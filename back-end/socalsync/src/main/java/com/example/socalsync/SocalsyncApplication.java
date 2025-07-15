a572f1c82a1a8a72390e10d4729a5938fef6db99
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-07-15 14:41:03 -0500

    initial setup of frontend and backend folders. Will begin to add components from origianl project.

---
+++ back-end/socalsync/src/main/java/com/example/socalsync/SocalsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.socalsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocalsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocalsyncApplication.class, args);
+	}
+
+}
---
+++ back-end/socalsync/src/test/java/com/example/socalsync/SocalsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socalsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocalsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
